d1b6c56637f7aa604b2e402f8508d756141137c6
Donghwan Lee <ehwan007@gmail.com>
2021-04-22 20:44:50 +0900

    chore: MongoDB 설정 변경, 정보 보호

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const config = require('./config/key');
 const { User } = require("./models/User"); 
 
 // application/x-www-from-urlencoded (코드를 분석해서 가져옴)
@@ -11,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://donghwan:9080@youtube-clone.gwiov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err))
